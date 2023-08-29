131b78eab654d5a977e5f4ab6a8499cf3db989e7
Naiguel Santos <naiguel.santos@objective.com.br>
2023-08-28 22:06:38 -0300

    rubocop indentation

--- lib/roman_calculator.rb
+++ lib/roman_calculator.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 class RomanCalculator
-    attr_reader :letter_a, :letter_b
+  attr_reader :letter_a, :letter_b
 
-    def initialize(letter_a, letter_b)
-        @letter_a = letter_a
-        @letter_b = letter_b
-    end
+  def initialize(letter_a, letter_b)
+    @letter_a = letter_a
+    @letter_b = letter_b
+  end
 
-    def add
-        letter_a + letter_b
-    end
+  def add
+    letter_a + letter_b
+  end
 end
